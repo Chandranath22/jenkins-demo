d56cfbca2755e11e56840fd2d91a972a2d2d591d
Chandranath Mondal <chandranath222@gmail.com>
2020-09-01 15:39:12 +0530

    First commit

---
+++ calculator/calculator.py
@@ -0,0 +1,117 @@
+import json
+# This function adds two numbers
+def add(x, y):
+  return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+  return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+  return x * y
+
+# This function divides two numbers
+def divide(x, y):
+  if (y == 0):
+    raise ValueError('Cannot divide by 0')
+  return x / y
+
+#This function checks if the input is a number
+def check_is_numeric (number):
+  if (number.isnumeric()):
+    return True
+  return False
+
+
+#Driver function
+def lambda_handler (event, context):
+
+    # Take input from the user
+    choice = event['pathParameters']['choice']
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+
+      #Get the numbers from user
+      first_num = event['pathParameters']['firstNumber']
+
+      #Check is number
+      if(check_is_numeric(first_num) == False):
+        print("Invalid input")
+        return {
+          'statusCode': 400,
+          'body': json.dumps({
+            'message': 'Invalid input',
+            'value': 'Bad input'
+          })
+        }
+
+      else:
+        num1 = float(first_num)
+
+      second_num = event['pathParameters']['secondNumber']
+
+      #Check is number
+      if(check_is_numeric(first_num) == False):
+        print("Invalid input")
+        return {
+          'statusCode': 400,
+          'body': json.dumps({
+            'message': 'Invalid input',
+            'value': 'Bad input'
+          })
+        }
+      
+      else:
+        num2 = float(second_num)
+        
+      if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+        return {
+          'statusCode': 200,
+          'body': json.dumps({
+            'message': 'Addition operation',
+            'value': add(num1, num2)
+          })
+        }
+
+      elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+        return {
+          'statusCode': 200,
+          'body': json.dumps({
+            'message': 'Subtraction operation',
+            'value': subtract(num1, num2)
+          })
+        }
+
+      elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+        return {
+          'statusCode': 200,
+          'body': json.dumps({
+            'message': 'Multiplication operation',
+            'value': multiply(num1, num2)
+          })
+        }
+
+      elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+        return {
+          'statusCode': 200,
+          'body': json.dumps({
+            'message': 'Division operation',
+            'value': divide(num1, num2)
+          })
+        }
+
+    else:
+        print("Invalid Input")
+        return {
+          'statusCode': 400,
+          'body': json.dumps({
+            'message': 'Invalid operation',
+            'value': 'Bad request'
+          })
+        }
---
+++ calculator/test_calc.py
@@ -0,0 +1,37 @@
+import unittest
+import calculator as calc
+
+class TestCalc (unittest.TestCase):
+  def test_add (self):
+    self.assertEqual(calc.add(43, 25), 68)
+    self.assertEqual(calc.add(-1, 1), 0)
+    self.assertEqual(calc.add(-1, -1), -2)
+
+  def test_subtract (self):
+    self.assertEqual(calc.subtract(43, 25), 18)
+    self.assertEqual(calc.subtract(-1, 1), -2)
+    self.assertEqual(calc.subtract(-1, -1), 0)
+
+  def test_multiply (self):
+    self.assertEqual(calc.multiply(43, 25), 1075)
+    self.assertEqual(calc.multiply(-1, 1), -1)
+    self.assertEqual(calc.multiply(-1, -1), 1)
+    
+  def test_divide (self):
+    self.assertEqual(calc.divide(10, 2), 5)
+    self.assertEqual(calc.divide(43, 25), 1.72)
+    self.assertEqual(calc.divide(-1, 1), -1)
+    self.assertEqual(calc.divide(-1, -1), 1)
+    with self.assertRaises(ValueError):
+      calc.divide(2, 0)
+
+  def test_numeric (self):
+    self.assertFalse(calc.check_is_numeric('-10'))
+    self.assertTrue(calc.check_is_numeric('10'))
+    self.assertFalse(calc.check_is_numeric('abc'))
+    self.assertFalse(calc.check_is_numeric('10abc'))
+    self.assertFalse(calc.check_is_numeric('abc10'))
+    self.assertFalse(calc.check_is_numeric('abc10asd'))
+
+if __name__ == '__main__':
+  unittest.main()
